ef2032422c98b4642517cdd6006322960a387851
salmaayad0 <salmaayad017@gmail.com>
2023-06-21 19:58:11 +0300

    sppiner load add

--- pdr-system/src/App.js
+++ pdr-system/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Blocks } from 'react-loader-spinner'
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Doctor from './pages/Doctor';
@@ -13,18 +13,42 @@ import Patient from './pages/Patient';
 function App() {
 
   useEffect(() => {
-  AOS.init(20000);
-}, [])
+    AOS.init(20000);
+  }, [])
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Layout />} >
-      <Route path='/' element={<Home />} />
-      <Route path='/doctor' element={<Doctor />}/>
-      <Route path='/patient' element={<Patient />}/>
-      </Route>
-    </Routes>
+      {loading ? (
+        <Blocks
+          visible={loading}
+          height="200"
+          width="200"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)"
+          }}
+          wrapperClass="blocks-wrapper"
+        />
+      ) : (
+        <Routes>
+          <Route path='/' element={<Layout />} >
+            <Route path='/' element={<Home />} />
+            <Route path='/doctor' element={<Doctor />} />
+            <Route path='/patient' element={<Patient />} />
+          </Route>
+        </Routes>
+      )}
     </>
   );
 }
--- pdr-system/src/index.js
+++ pdr-system/src/index.js
@@ -5,15 +5,18 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux'
+import { store } from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
