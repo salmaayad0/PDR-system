5a82e65f9be57962702d8e7fe9411f65384d36c2
salmaayad0 <salmaayad017@gmail.com>
2023-06-29 21:01:11 +0300

    pull req

--- PDR/PDR/settings.py
+++ PDR/PDR/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'PDR.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pdr',
         'USER': 'admin',
