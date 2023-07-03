9cf7e7e1bfee43a3f1e4e449fe8d9839002269b9
eman183 <eman.mohamed1832000@gmail.com>
2023-07-03 21:41:19 +0300

    pull request

--- PDR/PDR/settings.py
+++ PDR/PDR/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {  'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-           'rest_framework.permissions.IsAdminUser', 
+        #    'rest_framework.permissions.IsAdminUser', 
          'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- PDR/patients/urls.py
+++ PDR/patients/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import delete_patient,update_patient,search_patient,LoginP,CreateSession,LogoutP,get_patient,UpdateSession,get_patient_by_email,registrationView,ListSession,CreateHistory,Listhistory,UpdateHistory,get_patient_by_phone,login
+from .views import delete_patient,update_patient,search_patient,get_all_patient,LoginP,CreateSession,LogoutP,get_patient,UpdateSession,get_patient_by_email,registrationView,ListSession,CreateHistory,Listhistory,UpdateHistory,get_patient_by_phone,login
 from rest_framework.generics import ListCreateAPIView
 from .models import Patients
 from .serializer import Patientselizer
 urlpatterns = [
 path('DeletePatient/<int:id>',delete_patient),
+path('allPatient/',get_all_patient),
+
 path('UpdatePatient/<int:id>',update_patient),
 path("search_patient/<phone_number>",search_patient),
 path("get_patient/<int:id>",get_patient),
@@ -19,7 +21,6 @@ path('addhistory/', CreateHistory.as_view()),
 path('GetHistory/<int:id>',Listhistory),
 path('UpdateHistory/<int:id>/',UpdateHistory),
 path('registration/', registrationView.as_view(), name='api_doctor_registration'),
-# path('trainerlogin/', trainerlogin),
 path("login",LoginP.as_view()),
 path("logout",LogoutP)
 
